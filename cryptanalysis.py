55ec23843e534c9a9e8decb98f98761ec6e8ca15
Arjun1202 <dbhxenor129@gmail.com>
2023-04-02 00:19:11 +0530

    Cryptanalysis added

---
+++ cryptanalysis.py
@@ -0,0 +1,138 @@
+from collections import Counter
+from flask import Flask,render_template,request
+from sympy import Matrix
+import numpy as np
+from math import ceil
+from numpy import array
+
+def monocrypt1(ctext,ptext):
+    plain_text=ptext.lower()
+    cipher_text=ctext.upper()
+    len1=len(plain_text)
+    len2=len(cipher_text)
+    if ((len1==len2) and (len2==26)):
+        unsorted_keys={}
+        ind=0
+        for p in plain_text:
+            unsorted_keys[p]=cipher_text[ind]
+            ind+=1
+        unsorted_keys=sorted(unsorted_keys.items())
+        key={}
+        for i,j in unsorted_keys:
+            key[i]=j
+        return key
+    else:
+        return "invalid!"
+
+def shiftcrypt1(ctext):
+    cipher_text=ctext.upper()
+    list_plaintext=[]
+    plain_key={}
+    for i in range(26):
+        plain_text=""
+        for c in cipher_text:
+            plain_text+=chr(((ord(c)%65-i)%26)+97)
+        list_plaintext.append(plain_text)
+        plain_key[plain_text]=i
+    words_list=open('wordslist.txt','r').read().split('\n')
+    l=len(cipher_text)
+    possible_words=[]
+    possible_plaintexts=[]
+    for word in list_plaintext:
+        for i in range(l):
+            j=i+4 #length of word>3
+            while j<l+1:
+                substring=word[i:j].lower()
+                if substring in words_list:
+                    possible_words.append(substring)
+                    possible_plaintexts.append(word)
+                j+=1
+    print(possible_plaintexts)
+    count=Counter(possible_plaintexts)
+    if(len(count)!=0):
+        for key,times in count.most_common(5):
+            print(key,"occurs",times,"times with key value",plain_key[key])
+        return [count.most_common()[0][0],plain_key[count.most_common()[0][0]]]
+    else:
+        return "invalid!"
+
+def affinecrypt1(ctext):
+    cipher_text=ctext.upper()
+    encryption_val={}
+    ind=0
+    while ind!=26:
+        encryption_val[chr(97+ind)]=ind
+        ind+=1
+    decryption_val={}
+    ind=0
+    while (ind!=26):
+        decryption_val[ind]=chr(65+ind)
+        ind+=1
+    list_plaintext=[] # holds every possible plaintext values
+    plain_keypairs={} # holds key pairs corresponding to every plaintext
+    inverse={1:1,3:9,5:21,7:15,9:3,11:19,15:7,17:23,19:11,21:5,23:17,25:25}
+    for key1 in inverse:
+        for key2 in range(0,26):
+            plain_text=""
+            for c in cipher_text:
+                plain_text+=decryption_val[((encryption_val[c.lower()]-key2)*(inverse[key1]))%26].lower()
+            list_plaintext.append(plain_text)
+            plain_keypairs[plain_text]=(key1,key2)
+    #print(list_plaintext)
+    words_list=open('wordslist.txt','r').read().split('\n')
+    l=len(cipher_text)
+    possible_words=[]
+    possible_plaintexts=[]
+    for word in list_plaintext:
+        for i in range(l):
+            j=i+4 #length of word>3
+            while j<l+1:
+                substring=word[i:j].lower()
+                if substring in words_list:
+                    possible_words.append(substring)
+                    possible_plaintexts.append(word)
+                j+=1
+    count=Counter(possible_plaintexts)
+    if(len(count)!=0):
+        for key,times in count.most_common():
+            print(key,"occurs",times,"times with key pair",plain_keypairs[key])
+        print("most common string:",count.most_common()[0][0])
+        print("key pair:", plain_keypairs[count.most_common()[0][0]])
+        return [count.most_common()[0][0],plain_keypairs[count.most_common()[0][0]]]
+    else:
+        return "invalid!"
+
+def hillcrypt1(ctext,ptext,keysize):
+    try:
+        plain_text=ptext.lower()
+        cipher_text=ctext.upper()
+        key_size=int(keysize)
+        plain_text=plain_text[:key_size*key_size]
+        cipher_text=cipher_text[:key_size*key_size]
+        plain_matrix=[]
+        row=[]
+        for p in plain_text:
+            row.append(ord(p)%97)
+            if len(row)==key_size:
+                plain_matrix.append(row)
+                row=[]
+        # print(array(plain_matrix))
+        cipher_matrix=[]
+        row=[]
+        for c in cipher_text:
+            row.append(ord(c)%65)
+            if len(row)==key_size:
+                cipher_matrix.append(row)
+                row=[]
+        # print(array(cipher_matrix))
+        invplain_matrix=Matrix(plain_matrix).inv_mod(26)
+        # print(array(invplain_matrix))
+        key_matrix=(Matrix(invplain_matrix)*Matrix(cipher_matrix)%26)
+        # print(array(key_matrix))
+        key=""
+        for k in list(key_matrix):
+            key+=chr(65+k)
+        return key
+    except Exception as e:
+        print(e)
+        return "invalid!"
---
+++ vigcryptanalysis.py
@@ -0,0 +1,125 @@
+from collections import defaultdict,deque
+import numpy as np
+from math import ceil
+import math
+from collections import Counter
+
+def kasiski(cipher_text,size=3):
+    possible_seq=defaultdict(list)
+    repeating_seq={}
+    pos=0
+    while(pos+size<=len(cipher_text)):
+        substr=cipher_text[pos:pos+size]
+        ind = [i for i in range(len(cipher_text)) if cipher_text.startswith(substr, i)]
+        possible_seq[substr]=ind
+        pos+=1
+    for key,val in possible_seq.items():
+        if len(val)>1:
+            repeating_seq[key]=val
+    #print(repeating_seq)
+    sd={}
+    j=""
+    initial=""
+    for i,val in repeating_seq.items() :
+        if j=="":
+            initial=i
+            j=i
+        elif count==val:
+            j+=i[-1]
+        else :
+            sd[j]=repeating_seq[initial]
+            j=i
+            initial=i
+        count=[x+1 for x in val]
+    sd[j]=repeating_seq[initial]
+    #print(sd)
+    diff=[]
+    for key,val in sd.items():
+        j=0
+        while(j+1<len(val)):
+            diff.append(val[j+1]-val[j])
+            j+=1
+    return diff
+    #possible_gcd=[]
+
+def divisors(n):
+    ret =[]
+    for i in range(1,math.ceil(math.sqrt(n))+1):
+        if n%i== 0:
+            if i>2: #since we consider key of length >=3 in kasiski method
+                ret.append(i)
+            if (i!=int(n/i)):
+                ret.append(int(n/i))
+    return ret
+
+def frequency_calculator(word):
+    frequency = { "a" : 0,  "b" : 0,  "c" : 0,  "d" : 0,  "e" : 0, "f" : 0,  "g" : 0,
+        "h" : 0,  "i" : 0,  "j" : 0,  "k" : 0,  "l" : 0,  "m" : 0,  "n" : 0,  "o" :   0,
+        "p" : 0,  "q" : 0,  "r" : 0,  "s" : 0,  "t" : 0,  "u" : 0,  "v" : 0,  "w" : 0,
+        "x" : 0,  "y" : 0,  "z" : 0 }
+    for letter in word:
+        frequency[letter]+= 1
+    for i in frequency:
+        frequency[i]=frequency[i]/len(word)*100
+    return frequency
+
+def product_value(actual_freq,stand_freq):
+    f1=deque(actual_freq.values())
+    f2=list(stand_freq.values())
+    sum_array=[]
+    for _ in range (len(f2)):
+        sum1=0
+        for i in range(len(f2)):
+            sum1+=f1[i]*f2[i]*100
+        sum_array.append(sum1)
+        f1.rotate(-1)
+    keychar=max(sum_array)
+    return chr(sum_array.index(keychar)+97)
+
+def vigcrypt1(ctext):
+    cipher_text=ctext
+    s= kasiski(cipher_text,3)
+    divisors_list=[]
+    for i in s:
+        divisors_list.extend(divisors(i))
+    possible_gcd=Counter(divisors_list)
+    key_length=2
+    v=0
+    for key,val in possible_gcd.most_common():
+        if key>key_length and v<=val:
+            key_length= key
+            v=val
+    print(key_length)
+    print(possible_gcd)
+    count=0
+    for x,y in possible_gcd.most_common(4):
+        key_length=x
+        print("Key length: ",x)
+        count+=1
+        collect={}
+        #print(collect)
+        for i in range(len(cipher_text)):
+            if (i%key_length not in collect):
+                collect[i%key_length]=[]
+            collect[i%key_length].append(cipher_text[i])
+        string_set={}
+        for i in range(key_length):
+            string_set[i]="".join(collect[i]).lower()
+        #print(string_set)
+        freq_set={}
+        for key,val in string_set.items():
+            freq_set[key]=frequency_calculator(val)
+        #print(freq_set)
+        keyset=[]
+        stand_freq={'A':8.2,'B':1.5,'C':2.8,'D':4.3,'E':12.7,'F':2.2,'G':2.0,'H':6.1,'I':7.0,'J':0.02,'K':0.08,'L':4.0,'M':2.4,'N':6.7,'O':7.5,'P':1.9,'Q':0.01,'R':6.0,'S':6.3,'T':9.1,'U':2.8,'V':1.0,'W':2.3,'X':0.01,'Y':2.0,'Z':0.01}
+        for key,val in freq_set.items():
+            keyset.append(product_value(val,stand_freq))
+        key="".join(keyset).upper()
+        plain_text=""
+        j=0
+        for c in cipher_text:
+            plain_text+=chr(97+(ord(c)%65-ord(key[j])%65)%26)
+            j=(j+1)%len(key)
+        print("key value:",key)
+        print(plain_text)
+        return [key,plain_text]
